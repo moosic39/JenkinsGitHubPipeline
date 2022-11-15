297ef30fde718f8a15d7b5013c296b99031b4fa2
Mickael Jegat <46851195+moosic39@users.noreply.github.com>
2022-11-15 12:34:52 +0100

    Update PatientIntakeApplication.java

--- src/main/java/com/ea/patientintake/PatientIntakeApplication.java
+++ src/main/java/com/ea/patientintake/PatientIntakeApplication.java
@@ -3,7 +3,7 @@ package com.ea.patientintake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-imprt com.ea.patientintake.model.ClinicMain;
+import com.ea.patientintake.model.ClinicMain;
 
 //Comment for poll
 @SpringBootApplication
